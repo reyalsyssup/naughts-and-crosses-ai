61d8d2b4b0c7c2f4e804e320c79fe94c5d1a42f7
reyalsyssup <63643459+reyalsyssup@users.noreply.github.com>
2020-06-26 16:19:35 +1000

    Start of refactoring, may have introduced some unkown bugs

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import ai
 import colorama
-from colorama import Fore, init
-init(autoreset=True)
+from colorama import Fore
+colorama.init(autoreset=True)
 
 # Set up the board as a 2d matrix array
 board = [
@@ -10,7 +10,6 @@ board = [
     [{"x": 0, "o": 0},{"x": 0, "o": 0},{"x": 0, "o": 0}]
 ]
 
-run = True
 player = 1
 useEnemyAI = False
 
@@ -20,47 +19,29 @@ def printWin(newBoard, playerNum):
     print(Fore.YELLOW + str(newBoard[1]))
     print(Fore.YELLOW + str(newBoard[2]))
     print(f"{Fore.GREEN}We have a winner, congratulations player {playerNum}!")
-    run = False
     quit()
 
 # Check win
 def checkWin(newBoard, playerNum):
-    # Checking if anyone has won
-    tlCheckingChar = newBoard[0][0]
-    drCheckingChar = newBoard[2][2]
-    mmCheckingChar = newBoard[1][1]
-    trCheckingChar = newBoard[0][2]
     for row in newBoard:
         for i in row:
-            if(tlCheckingChar != " "):
+            if(newBoard[0][0] != " "):
                 # TL to TR
-                if(newBoard[0][1] == tlCheckingChar and newBoard[0][2] == tlCheckingChar):
-                    printWin(newBoard, playerNum)
-                # TL to DL
-                elif(newBoard[1][0] == tlCheckingChar and newBoard[2][0] == tlCheckingChar):
-                    printWin(newBoard, playerNum)
-                # Left to right diag
-                # TL to DR
-                elif(newBoard[1][1] == tlCheckingChar and newBoard[2][2] == tlCheckingChar):
-                    printWin(newBoard, playerNum)
-            if(drCheckingChar != " "):    
+                if((newBoard[0][1] == newBoard[0][0] and newBoard[0][2] == newBoard[0][0]) or (newBoard[1][1] == newBoard[0][0] and newBoard[2][2] == newBoard[0][0]) or (newBoard[1][0] == newBoard[0][0] and newBoard[2][0] == newBoard[0][0])):
+                    printWin(newBoard, playerNum); return
+            if(newBoard[2][2] != " "):    
                 # All wins from DR (Down Right)
                 # DR to TR
-                if(newBoard[1][2] == drCheckingChar and newBoard[0][2] == drCheckingChar):
-                    printWin(newBoard, playerNum)
-                # DR to DL
-                elif(newBoard[2][1] == drCheckingChar and newBoard[2][0] == drCheckingChar):
-                    printWin(newBoard, playerNum)
-            if(mmCheckingChar != " "):
+                if((newBoard[1][2] == newBoard[2][2] and newBoard[0][2] == newBoard[2][2]) or (newBoard[2][1] == newBoard[2][2] and newBoard[2][0] == newBoard[2][2])):
+                    printWin(newBoard, playerNum); return
+            if(newBoard[1][1] != " "):
                 # Up, down and left, right wins
-                if(newBoard[0][1] == mmCheckingChar and newBoard[2][1] == mmCheckingChar):
-                    printWin(newBoard, playerNum)
-                elif(newBoard[1][0] == mmCheckingChar and newBoard[1][2]== mmCheckingChar):
-                    printWin(newBoard, playerNum)
-            if(trCheckingChar != " "):
+                if((newBoard[0][1] == newBoard[1][1] and newBoard[2][1] == newBoard[1][1]) or (newBoard[1][0] == newBoard[1][1] and newBoard[1][2]== newBoard[1][1])):
+                    printWin(newBoard, playerNum); return
+            if(newBoard[0][2] != " "):
                 # TR to DL
-                if(newBoard[1][1] == trCheckingChar and newBoard[2][0] == trCheckingChar):
-                    printWin(newBoard, playerNum)
+                if(newBoard[1][1] == newBoard[0][2] and newBoard[2][0] == newBoard[0][2]):
+                    printWin(newBoard, playerNum); return
 
 # Check if board is full
 def fullCheck(newBoard):
@@ -87,47 +68,42 @@ def fillBoard():
 
 # Render Board
 def renderBoard():
-    global run
-    newBoard = fillBoard()
-
     # Checking if board is full in which case end the game
-    fullCheck(newBoard)
-
+    fullCheck(fillBoard())
     # prints board
-    print(Fore.YELLOW + str(newBoard[0]))
-    print(Fore.YELLOW + str(newBoard[1]))
-    print(Fore.YELLOW + str(newBoard[2]))
+    print(Fore.YELLOW + str(fillBoard()[0]))
+    print(Fore.YELLOW + str(fillBoard()[1]))
+    print(Fore.YELLOW + str(fillBoard()[2]))
 
-def updateBoard(playerSymbol, loc):
+def updateBoard(playerNum, loc):
     global player
     # Makes sure location input is in the correct format
     try: loc[0], loc[1] = int(loc[0]), int(loc[1])
     except:
         print(f"{Fore.RED}Invalid Input")
-        player = player%2+1
         return False
     boardLoc = board[loc[0]][loc[1]]
     # Checks if player 1/2 is playing and making sure there isnt already a mark in their desired spot
-    if playerSymbol == 1 and (boardLoc["x"] == 0 and boardLoc["o"] == 0):
-        boardLoc["x"] = 1
-    elif(boardLoc["o"] == 0 and boardLoc["x"] == 0):
-        boardLoc["o"] = 1
+    if(playerNum == 1 and (boardLoc["x"] == 0 and boardLoc["o"] == 0)): boardLoc["x"] = 1
+    elif(boardLoc["o"] == 0 and boardLoc["x"] == 0): boardLoc["o"] = 1
     else:
         # If there is a mark in their spot, then tell them
         print(f"{Fore.RED}All ready a mark in that spot. Select another!")
         # Make sure the player doesnt change
-        player = player%2+1
+        player = playerNum
+        return
+    checkWin(fillBoard(), playerNum)
+    player = player%2+1
 
 def useEnemyAIPrompt():
     global useEnemyAI
-    print(f"{Fore.RED}Play with AI? y/n > ", end="")
-    useEnemyAI = input()
+    print(f"{Fore.RED}Play with AI? y/n > ", end=""); useEnemyAI = input()
     if(useEnemyAI == "y"): useEnemyAI = True
+    else: useEnemyAI = False
 
 def restartOnFullCheck():
     global board
     global player
-    global useEnemyAI
     # If the board is full there's no point in continuing, just quit the app
     if(fullCheck(fillBoard()) == True):
         renderBoard()
@@ -146,20 +122,14 @@ def restartOnFullCheck():
 
 if(__name__ == "__main__"):
     useEnemyAIPrompt()
-    print(f"{Fore.MAGENTA}Player 1: x\nPlayer 2: 0\n")
-    while run:
-        restartOnFullCheck()
+    print(f"{Fore.MAGENTA}Player 1: x\nPlayer 2: o\n")
+    while True:
         renderBoard()
+        restartOnFullCheck()
         print(f"{Fore.MAGENTA}Player {player}, where do you go? y x > ", end=""); loc = input()
-        if(loc == "exit"):
-            run = False
-            quit()
+        if(loc == "exit"): quit()
         loc = list(loc.split(" "))
         updateBoard(player, loc)
-        checkWin(fillBoard(), player)
-        player = player%2+1
         restartOnFullCheck()
-        if(useEnemyAI == True and player == 2):
-            updateBoard(player, ai.move(board))
-            player = player%2+1
-            checkWin(fillBoard(), 2)
+        if(useEnemyAI and player == 2):
+            updateBoard(2, ai.move(board))
